d6c2b7fe37d59b569110591705802c0317a1ad37
matheus <matheusgomes1203@hotmail.com>
2020-12-08 22:19:25 -0300

    Modificando caminho das pastas

--- settings.js
+++ settings.js
@@ -2,17 +2,17 @@
 const settings = {
   // src folder
   src: {
-    source: __dirname + '/src/', // source folder
-    images: __dirname + '/src/img/', // images
-    js: __dirname + '/src/js/', // javascripts
-    sass: __dirname + '/src/sass/', // sass
+    source: '../src/', // source folder
+    images: '../src/img/', // images
+    js: '../src/js/', // javascripts
+    sass: '../src/sass/', // sass
   },
 
   // dist folder
   dist: {
-    css: __dirname + '/dist/css/', // stylesheets
-    js: __dirname + '/dist/js/', // javascripts
-    images: __dirname + '/dist/img/', // images
+    css: '../dist/css/', // stylesheets
+    js: '../dist/js/', // javascripts
+    images: '../dist/img/', // images
   },
 };
 
